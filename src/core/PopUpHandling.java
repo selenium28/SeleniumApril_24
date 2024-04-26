f9b3eed63a2e36cc592ad1f28df58046d4ec2c8b
selenium28 <devselenium.class2819@gmail.com>
2024-04-26 17:59:39 +0530

    waits

--- src/core/PopUpHandling.java
+++ src/core/PopUpHandling.java
@@ -1,10 +1,14 @@
 package core;
 
+import java.time.Duration;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PopUpHandling {
 
@@ -13,15 +17,17 @@ public class PopUpHandling {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demo.guru99.com/test/delete_customer.php");
 		driver.manage().window().maximize();
-		Thread.sleep(10000);
+		WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(20));
 		
 		WebElement cusID = driver.findElement(By.name("cusid"));
 		cusID.sendKeys("5434");
 		
 		WebElement submitButton = driver.findElement(By.xpath("//input[@type='submit']"));
+		wait.until(ExpectedConditions.elementToBeClickable(submitButton));
 		submitButton.click();
 		
 //		Alert - interface in selenium
+		wait.until(ExpectedConditions.alertIsPresent());
 		Alert alert = driver.switchTo().alert();
 		String popmsg = alert.getText();
 		Thread.sleep(3000);
--- src/waits/SeleniumWaits.java
+++ src/waits/SeleniumWaits.java
@@ -1,12 +1,14 @@
 package waits;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumWaits {
@@ -15,10 +17,12 @@ public class SeleniumWaits {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.manage().window().maximize();
-//		1. Implicit Wait  - Global Wait
+//		1. Implicit Wait  - Global Wait   
+//		Default time for Implicit Wait - Zero  - it keeps polling every 500 milisec. min sec
 //		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 //		2. Explicit Wait - Dynamic Wait
+//		Default polling time - 0.5sec
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		wait.until(ExpectedConditions.titleContains("OrangeHRM"));
 		WebElement username = driver.findElement(By.name("username"));
@@ -26,6 +30,13 @@ public class SeleniumWaits {
 		
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//*[@type='submit']")).click();
+		
+//		3. Fluent Wait
+		FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver);
+		fwait.withTimeout(Duration.ofSeconds(30))
+		.pollingEvery(Duration.ofSeconds(1))
+		.ignoring(NoSuchElementException.class);
+		
 		
 	}
 
